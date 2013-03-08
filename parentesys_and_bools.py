66bb0761c91f4f78fba596eb7650117ae4c7e96a
Filipe Ximenes <filipeximenes@gmail.com>
2013-03-07 22:57:25 -0300

    refactoring

--- parentesys_and_bools.py
+++ parentesys_and_bools.py
@@ -20,30 +20,21 @@ import itertools
 
 def get_item(exp, pos):
     for j, tup in enumerate(exp):
-        i, l = tup
-        if i == pos:
-            return j
+        if tup[0] == pos: return j
 
 
 def do_op(bool1, op, bool2):
-    if op == 'and':
-        return eval(bool1) and eval(bool2)
-    elif op == 'or':
-        return eval(bool1) or eval(bool2)
-    elif op == 'xor':
-        return eval(bool1) != eval(bool2)
+    if op == 'and': return eval(bool1) and eval(bool2)
+    elif op == 'or': return eval(bool1) or eval(bool2)
+    elif op == 'xor': return eval(bool1) != eval(bool2)
 
 
 def operate(exp, item):
     i = get_item(exp, item)
     if i:
-        j, exp1 = exp[i-1]
-        j, op = exp[i]
-        j, exp2 = exp[i+1]
-
-        exp.pop(i-1)
-        exp.pop(i-1)
-        exp.pop(i-1)
+        _, exp1 = exp.pop(i-1)
+        _, op = exp.pop(i-1)
+        _, exp2 = exp.pop(i-1)
         exp.insert(i-1, (-1, str(do_op(exp1, op, exp2))))
 
     return exp
@@ -55,12 +46,9 @@ def count_par(exp, v=False):
     exp = exp.split()
     exp = zip(range(len(exp)), exp)
 
-    if v:
-        print exp
-        print 'precedence order:'
+    if v: print exp; print 'precedence order:'
 
     ops = [pos for pos, item in exp[1::2]]
-
     ops = itertools.permutations(ops, len(ops))
 
     for sequence in ops:
@@ -68,12 +56,10 @@ def count_par(exp, v=False):
         for op in sequence:
             aux_exp = operate(aux_exp, op)
 
-        i, res = aux_exp[0]
-        if res == 'True':
-            count += 1
+        _, res = aux_exp[0]
+        if res == 'True': count += 1
 
-        if v:
-            print sequence
+        if v: print sequence
 
     return count
 
